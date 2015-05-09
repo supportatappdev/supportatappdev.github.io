013fb0cb612f727957f8efccaa522422b3a9e258
SrikanthIO <srikanth.akkapu@cloudio.io>
2015-05-09 17:20:57 +0530

    Added Hellow World App1

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,26 @@
-console.log('\'Allo \'Allo!');
+var dhelp = angular.module('dhelp',['ngRoute']);
+
+dhelp.config(['$routeProvider',
+  function($routeProvider) {
+  	   $routeProvider.otherwise("/");
+    $routeProvider.
+      when('/', {
+        templateUrl: 'views/hm.html',
+        controller: 'DHelpCtrl'
+      }).when('/hw1', {
+        templateUrl: 'views/hw1.html',
+        controller: 'HW1Ctrl'
+      })
+  }]);
+
+dhelp.controller('DHelpCtrl',function($scope,$rootScope,$location){
+	console.log("************ IN HELP CONTROLLER***********");
+	$scope.go = function(path){
+		$location.path(path)
+	};
+});
+
+dhelp.controller('HW1Ctrl',function($scope,$rootScope,$location){
+	console.log("************ IN Hellow World CONTROLLER***********");
+	
+});
