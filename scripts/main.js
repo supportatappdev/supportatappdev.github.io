24ed4af85314abe66ca53839d6868fb5a68d9420
SrikanthIO <srikanth.akkapu@cloudio.io>
2015-05-10 23:46:42 +0530

    completed Hellow world app1

--- scripts/main.js
+++ scripts/main.js
@@ -10,11 +10,21 @@ dhelp.config(['$routeProvider',
       }).when('/hw1', {
         templateUrl: 'views/hw1.html',
         controller: 'HW1Ctrl'
+      }).when('/aut', {
+        templateUrl: 'views/aut.html',
+        controller: 'AutCtrl'
       })
-  }]);
+  }]); 
 
 dhelp.controller('DHelpCtrl',function($scope,$rootScope,$location){
 	console.log("************ IN HELP CONTROLLER***********");
+  $scope.topics = [
+                {"idx":1,"name":"Create Hellow World App","link":"hw1"},
+                {"idx":2,"name":"Appying UI Themes","link":"aut"},
+                {"idx":3,"name":"Import/Export data","link":"ie"},
+                {"idx":4,"name":"API Console","link":"ac"}
+               ]
+
 	$scope.go = function(path){
 		$location.path(path)
 	};
@@ -22,5 +32,4 @@ dhelp.controller('DHelpCtrl',function($scope,$rootScope,$location){
 
 dhelp.controller('HW1Ctrl',function($scope,$rootScope,$location){
 	console.log("************ IN Hellow World CONTROLLER***********");
-	
 });
